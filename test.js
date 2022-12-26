1dbcdd7ef4904984a0e853f22251bc1a048de6c0
イトウチカノブ <go_tanfx@yahoo.co.jp>
2022-12-27 02:04:43 +0900

    不具合修正

--- test.js
+++ test.js
@@ -6,12 +6,13 @@ const ref = require("ref-napi");
 function filedirPlatform(){
     const is_windows = process.platform==='win32'
     const is_mac = process.platform==='darwin'
-    const pathDat = __filename;
+    let libname = "";
     if(is_windows){
       libname = process.cwd() + "/" + "libTest.dll"
       return libname
     } else if(is_mac){
-      libname = GetAppRoot() + "libTest.dylib"
+      res = GetAppRoot() 
+      libname = res[0] + "libTest.dylib"
       return libname
     } else {
       libname = process.cwd() + "/" + "libTest.so"
@@ -28,3 +29,34 @@ var ffilib = ffi.Library(result,{
 
 let mesResult = ffilib.ffiCheck("hello");
 console.log("calldata Message > " + mesResult);
+
+function GetAppRoot(){
+  let nextPath = __filename;
+  cutname,_ = GetCurrentName(nextPath)
+  let logpath = "First " + nextPath + " / " + cutname + os.EOL;
+  let appsPathArray = SearchPath("Contents",__filename,logpath)
+  const verPath = appsPathArray[0]
+  const exePath = path.normalize(verPath + '/../../')
+  const logPath = appsPathArray[1] + verPath + os.EOL + exePath + os.EOL + filename
+
+  //DebugLog
+  //const savePath = exePath + "log.txt"
+  //logSave(savePath,logPath)
+  return [verPath,exePath]
+}
+
+//対象の文字列のディレクトリ名が出るまで検索ß
+function SearchPath(searchStr,nextPath,log){
+  nextPath = path.normalize(nextPath + '/../');
+  cutname = GetCurrentName(nextPath)
+  if(cutname == ""){
+    return [nextPath,log]
+  }
+  log = log + nextPath + " / " + cutname + os.EOL;
+  if(cutname != searchStr){
+      const array = SearchPath(searchStr,nextPath,log)
+      nextPath = array[0]
+      log = array[1]
+  }
+  return [nextPath,log]
+}
