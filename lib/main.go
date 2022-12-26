d100ab3a06582e1678f4f67da9b51a239194d224
イトウチカノブ <go_tanfx@yahoo.co.jp>
2022-12-27 03:42:59 +0900

    返り値にStruct追加

--- lib/main.go
+++ lib/main.go
@@ -11,30 +11,32 @@ func ffiCheck(mes *C.char) *C.char {
 	result := fetch(db, id)
 
 	tex := C.GoString(mes)
-	return C.CString(tex + result)
+	return C.CString(tex + result.Bookname)
 }
 
 func main() {
 }
 
 type BookDatabase struct{}
-type Book struct{}
+type Book struct {
+	Bookname string "hogehoge"
+}
 
 func NewBookDatabase() *BookDatabase {
 	var db BookDatabase
 	return &db
 }
 
-func (d *BookDatabase) GetBook(id string) string {
-	return "ok"
+func (d *BookDatabase) GetBook(id string) *Book {
+	return &Book{Bookname: "okok"}
 }
 
 func (d *BookDatabase) CreateBook(data interface{}) {}
 
 type BookReader interface {
-	GetBook(string) string
+	GetBook(string) *Book
 }
 
-func fetch(b BookReader, id string) string {
+func fetch(b BookReader, id string) *Book {
 	return b.GetBook(id)
 }
